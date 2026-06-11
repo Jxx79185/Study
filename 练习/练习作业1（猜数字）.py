fd01afe146913d919ac568c502dc465cb20553ae
jiang <1281545400@qq.com>
2026-06-11 23:30:52 +0900

    new

--- 练习/练习作业1（猜数字）.py
+++ 练习/练习作业1（猜数字）.py
@@ -3,10 +3,13 @@ import random
 n=random.randint(1,100)
 
 prompt='请输入你猜的值：'
+print('猜数字：请输入1-100之中的值')
 while True:
-    i=int(input(prompt))
     try:
-        
+        i=int(input(prompt))
+        if i<0 or i >100:
+            print('请输入1-100以内的数字')
+            continue
         if i<n:
             prompt='猜错了，请再试一次，这次往更大的数字猜：'
         elif i>n:
